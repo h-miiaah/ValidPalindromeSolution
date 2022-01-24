b768195108c5486fe0117541817a1f951f79e3e8
h-miiaah <haim.miiaah5@gmail.com>
2022-01-23 21:36:15 -0500

    comments

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -37,6 +37,7 @@
   - First check if the string is empty. If it is, we return true.
   - Then convert the string to lowercase and iterate over it.
   - Check if the character is a letter or a number. If it is not, we continue to the next character.
+  - If the character is a letter or a number, we compare it to the character at the other end of the string.
   
 
   */

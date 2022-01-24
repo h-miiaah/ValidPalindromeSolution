4559211e7935a44ea81e1f813f29e6fea65d4ca2
h-miiaah <haim.miiaah5@gmail.com>
2022-01-23 21:32:46 -0500

    increment i

--- ValidPalindrome.js
+++ ValidPalindrome.js
@@ -12,17 +12,19 @@
         if ((s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9')) {
             i++;
             continue;
-          }
+            }
 
         if ((s[j] < 'a' || s[j] > 'z') && (s[j] < '0' || s[j] > '9')) {
             j--;
             continue;
-        }
+            }
 
         if (s[i] !== s[j]) {
             return false;
-          }
+            }
 
+        i++;
+        
       }
 
   };
